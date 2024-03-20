c9ef9d7059794b30fee66fa43788d817757ac330
peferetin <peferetin@yahoo.com>
2024-03-20 10:40:53 +0100

    Added some more

---
+++ data/movieData.js
@@ -0,0 +1,25 @@
+const myMovies = [
+  {
+    id: 1,
+    title: "The Bodyguard",
+    year: "1998",
+  },
+  {
+    id: 2,
+    title: "Scar-face",
+    year: "1994",
+  },
+  {
+    id: 3,
+    title: "Heat",
+    year: "2000",
+  },
+
+  {
+    id: 4,
+    title: "Terminator",
+    year: "1998",
+  },
+];
+
+export default myMovies;
--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 import express from "express";
 import "dotenv/config";
+import myMovies from "./data/movieData.js";
 
 const app = express();
 
-const myMovies = [
-  {
-    id: 1,
-    title: "The Bodyguard",
-    year: "1998",
-  },
-  {
-    id: 2,
-    title: "Scar-face",
-    year: "1994",
-  },
-  {
-    id: 3,
-    title: "Heat",
-    year: "2000",
-  },
+// movies got imported from movieData.js
 
-  {
-    id: 4,
-    title: "Terminator",
-    year: "1998",
-  },
-];
+// const myMovies = [
+//   {
+//     id: 1,
+//     title: "The Bodyguard",
+//     year: "1998",
+//   },
+//   {
+//     id: 2,
+//     title: "Scar-face",
+//     year: "1994",
+//   },
+//   {
+//     id: 3,
+//     title: "Heat",
+//     year: "2000",
+//   },
+
+//   {
+//     id: 4,
+//     title: "Terminator",
+//     year: "1998",
+//   },
+// ];
 app.get("/", (request, response) => response.send("hello World"));
 console.log(myMovies);
 
@@ -49,6 +52,12 @@ app.get("/movies/:id", (request, response) => {
   //   } else {
   //     response.status(404).send("Movie not found");
   //   }
+});
+
+app.post("/movies", (request, response) => {
+  const movie = request.body;
+  myMovies.push(movie);
+  response.send(movie);
 });
 
 app.listen(3000, () => {
