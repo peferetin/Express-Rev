8fd97816ea9d4902d871f16019623d67ce58c4e6
peferetin <peferetin@yahoo.com>
2024-03-21 15:27:29 +0100

    Finished the Test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import "dotenv/config";
 import myMovies from "./data/movieData.js";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -36,6 +38,17 @@ app.post("/movies", (request, response) => {
   console.log(myMovies);
 });
 
+app.put("/movies/:id", (request, response) => {
+  const id = request.params.id;
+  const movie = myMovies.find((movie) => movie.id == id);
+  if (movie) {
+    movie.title = request.body.title;
+    movie.year = request.body.year;
+    response.json(movie);
+  } else {
+    response.status(404).send("Movie not found");
+  }
+});
 app.listen(port, () => {
   console.log("Server is running on port 8000 ");
 });
